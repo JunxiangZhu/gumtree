4078e725af6c7a5043eec7a9f56bb465fda5bcd7
Junxiang <dingding41102@gmail.com>
2016-08-01 14:58:06 +0800

    fixed small bugs

--- gumtree/ItemCheck.m
+++ gumtree/ItemCheck.m
@@ -4,7 +4,7 @@ function y = ItemCheck(url)
 %Version: 1.1
 % compared with version 1.0, the interested areas are more extracted more
 % efficiently.
-%Date: 21-May-2016
+%Date: 1-Aug-2016
 %Email:junxiang.zhu@curtin.edu.au
 if nargin ~= 1, error('Wrong input argument.'); end
 
@@ -15,61 +15,39 @@ patern_title = '<span itemprop="name">\s*.*?</span>';
 result_title = regexp(str,patern_title,'match');
 result_title = result_title';
 for i = 1:length(result_title)
-result_title{i} = regexprep(result_title{i},'\s{2,}','');%eliminate blank
-result_title{i} = regexprep(result_title{i},'&#034;','"');%turn error into "
-result_title{i} = regexprep(result_title{i},'&#039;','?');%turn error into ?
-result_title{i} = regexprep(result_title{i},'&amp;','&');%turn error into &
-result_title{i} = result_title{i}(23:end-7);% extract information needed
+    result_title{i} = regexprep(result_title{i},'\s{2,}','');%eliminate blank
+    result_title{i} = regexprep(result_title{i},'&#034;','"');%turn error into "
+    result_title{i} = regexprep(result_title{i},'&#039;','?');%turn error into ?
+    result_title{i} = regexprep(result_title{i},'&amp;','&');%turn error into &
+    result_title{i} = result_title{i}(23:end-7);% extract information needed
 end
 
 % extract price
 patern_price = '<div class="j-ad-price.*?</div>';
-result_price = regexp(str,patern_price,'match');
-result_price = result_price';
+result_price = regexp(str,patern_price,'match')';
 for i = 1:length(result_price)
-kk = regexprep(result_price{i},'\s{2,}','');
-kk = regexprep(kk,'<.*?>','');
-result_price{i} = regexprep(kk,'\s','/');
-end
-
-%date
-patern_date = '<div class="rs-ad-date">.*?</div>';
-result_date = regexp(str,patern_date,'match');
-result_date = result_date';
-for i = 1:length(result_date)
-result_date{i} = regexprep(result_date{i},'\s{2,}','');%eliminate blank
-result_date{i} = result_date{i}(25:end-6);
+    kk = regexprep(result_price{i},'\s{2,}','');
+    kk = regexprep(kk,'<.*?>','');
+    result_price{i} = regexprep(kk,'\s','/');
 end
 
 %location (new version)
-patern_location = '<div class="rs-ad-location-area">.*?<div class="rs-ad-date">';
+patern_location = '<div class="ad-listing__location">.*?</div>';
 result_location = regexp(str,patern_location,'match')';
-rst_temp = cell(length(result_location),1);
 for i = 1:length(result_location)
-    
-    temp = result_location{i};
-    temp = regexprep(temp,'\s{2,}',''); %eliminate continuous space
-    ind1 = strfind(temp,'<');
-    ind2 = strfind(temp,'>');
-    if isempty(strfind(temp,'suburb'))
-        rst_temp{i} = [temp(ind2(1)+1:ind1(2)-1) '/------' ];
-        
-    else
-        rst_temp{i} = [temp(ind2(1)+1:ind1(2)-1) '/' temp(ind2(5)+1:ind1(6)-1)];
-    end
-    
+    kk = regexprep(result_location{i},'\s{2,}','');
+    result_location{i} = regexprep(kk,'<.*?>','');
 end
-result_location = rst_temp;
+
 %
 % link for each item
 patern_link = '<a itemprop="url".*?">';
-result_link = regexp(str,patern_link,'match');
-result_link = result_link';
-result_link = unique(result_link,'stable');
+result_link = regexp(str,patern_link,'match')';
 for i = 1:length(result_link)
     ind1 = strfind(result_link{i},'"/');
     result_link{i} = ['http://www.gumtree.com.au' result_link{i}(ind1+1:end-2)];
 end
+result_link = unique(result_link,'stable');
 
 %
 result = [result_title,result_price,result_location,result_link];
